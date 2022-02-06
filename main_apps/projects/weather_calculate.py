cbcabd123363c8475bf8d27c65e2a420994a2bc4
AlexVBodrov <alexvbodrov@mail.ru>
2022-02-07 00:30:54 +0300

    add weather module

--- main_apps/projects/weather_calculate.py
+++ main_apps/projects/weather_calculate.py
@@ -1,3 +1,17 @@
 import pyowm
+from pyowm import OWM
+from pyowm.utils import config
+from pyowm.utils import timestamps
 
-owm = OWM('your free OWM API key')
+owm = OWM('b3e3f2a737d2967e2851a9892592d8c7')
+mgr = owm.weather_manager()
+
+
+# Search for current weather in London (Great Britain) and get details
+city = 'Москва'
+observation = mgr.weather_at_place(city)
+w = observation.weather
+temperature = w.temperature('celsius')['temp']
+status = w.detailed_status
+print(f"В городе {city}  температура = {temperature} по Цельсию и статус {status}")
+
